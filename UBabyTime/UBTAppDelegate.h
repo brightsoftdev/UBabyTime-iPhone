b1a2fe7db9247544ad134309de4e549ad2198553
Jiang Zhimin <jiangzm@JiangmatoiMac.local>
2012-03-18 15:40:01 +0800

    Initial Commit

---
+++ UBabyTime/UBTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  UBTAppDelegate.h
+//  UBabyTime
+//
+//  Created by Zhimin Jiang on 12-3-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UBTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ UBabyTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UBabyTime
+//
+//  Created by Zhimin Jiang on 12-3-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UBTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UBTAppDelegate class]));
+    }
+}
